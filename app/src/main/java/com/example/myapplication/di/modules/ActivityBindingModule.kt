4d8bb3ffd0dc3723d8f5cd20b5004033ee6f1b27
rfmussa <rfmussa@gmail.com>
2019-05-09 20:15:34 +0200

    remove unnecessary classes

--- app/src/main/java/com/example/myapplication/di/modules/ActivityBindingModule.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.di.modules
-
-import com.example.myapplication.MainActivity
-import dagger.Module
-
-@Module
-abstract class ActivityBindingModule {
-	abstract fun providesMainActivity(): MainActivity
-}
--- app/src/main/java/com/example/myapplication/di/modules/ActivityModule.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.di.modules
-
-import com.example.myapplication.MainActivity
-import dagger.Module
-
-@Module
-abstract class ActivityModule {
-	abstract fun contributeMainActivity(): MainActivity
-}
